d8af1fb999355f68b873e54093cdc64ecb105a4c
icerda <icerda@senadis.cl>
2022-12-23 19:10:26 -0300

    Se cargan reglas del negocio por parte de la municipalidad

--- database/seeders/ReglasFormularioMensajeSeeder.php
+++ database/seeders/ReglasFormularioMensajeSeeder.php
@@ -16,8 +16,46 @@ class ReglasFormularioMensajeSeeder extends Seeder
      */
     public function run()
     {
-        ReglasFormularioMensaje::create(['id_regla' => '1', 'configuracion_mensaje'=>'required','mensaje'=>'Campo Nombre no puede ser vacio']);
-        ReglasFormularioMensaje::create(['id_regla' => '1', 'configuracion_mensaje'=>'max','mensaje'=>'Campo Nombre no puede exceder los 2000 caracteres']);
-        ReglasFormularioMensaje::create(['id_regla' => '2', 'configuracion_mensaje'=>'required','mensaje'=>'El campo cargo no puede ser vacio']);
+        /** MÓDULO 1: IDENTIFICACIÓN DEL INFORMANTE */
+        ReglasFormularioMensaje::create(['id_regla' => '1', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°1 obligatoria: Campo Nombre no puede ser vacio']);
+        ReglasFormularioMensaje::create(['id_regla' => '1', 'configuracion_mensaje'=>'max','mensaje'=>'Pregunta N°1 excede caracteres: Campo Nombre no puede exceder los 1000 caracteres']);
+        ReglasFormularioMensaje::create(['id_regla' => '2', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°2 obligatoria: Debe indicar un cargo.']);
+        ReglasFormularioMensaje::create(['id_regla' => '2', 'configuracion_mensaje'=>'max','mensaje'=>'Pregunta N°2: excede caracteres, Campo cargo no puede exceder los 1000 caracteres.']);
+        ReglasFormularioMensaje::create(['id_regla' => '3', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°3 obligatoria: Debe indicar un Correo Eléctronico']);
+        ReglasFormularioMensaje::create(['id_regla' => '3', 'configuracion_mensaje'=>'max','mensaje'=>'Pregunta N°3 excede caracteres: Campo email no puede exceder los 1000 caracteres']);
+        ReglasFormularioMensaje::create(['id_regla' => '3', 'configuracion_mensaje'=>'email','mensaje'=>'Pregunta N°3: Correo electrónico no cumple con el formato']);
+        ReglasFormularioMensaje::create(['id_regla' => '4', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°4 obligatoria: Debe indicar un Teléfono de contacto']);
+        
+        /** MÓDULO 2: SELECCIÓN PREFERENTE */
+        ReglasFormularioMensaje::create(['id_regla' => '5', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°5 obligatoria: Si no se han realizado procesos de selección y/o concursos indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '5', 'configuracion_mensaje'=>'min','mensaje'=>'Pregunta N°5 valor mínimo 0: Si no se han realizado procesos de selección y/o concursos indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '6', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°6 obligatoria: Si no hubo postulantes en procesos de selección y/o concursos indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '6', 'configuracion_mensaje'=>'min','mensaje'=>'Pregunta N°6 valor mínimo 0: Si no hubo postulantes en procesos de selección y/o concursos indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '7', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°7 obligatoria: Si no se hubo postulantes con discapacidad y/o asignatarios de pensión de invalidez en procesos de selección y/o concursos indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '7', 'configuracion_mensaje'=>'min','mensaje'=>'Pregunta N°7 valor mínimo 0: Si no se hubo postulantes con discapacidad y/o asignatarios de pensión de invalidez en procesos de selección y/o concursos indicar 0']);        
+        ReglasFormularioMensaje::create(['id_regla' => '7', 'configuracion_mensaje'=>'lte','mensaje'=>'Pregunta N°7: Se ha detectado una inconsistencia con el dato reportado en pregunta 6. El número  de postulantes con discapacidad y/o asignatarios de pensión de invalidez debe ser menor o igual al número total de postulantes a los procesos de selección']);
+        ReglasFormularioMensaje::create(['id_regla' => '8', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°8 obligatoria: Si no hubo postulantes con discapacidad y/o asignatarios de pensión de invalidez en una nómina final indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '8', 'configuracion_mensaje'=>'min','mensaje'=>'Pregunta N°8 valor mínimo 0: Si no hubo postulantes con discapacidad y/o asignatarios de pensión de invalidez en una nómina final indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '8', 'configuracion_mensaje'=>'lte','mensaje'=>'Pregunta N°8: Se ha detectado una inconsistencia con el dato reportado en pregunta 7: el total de postulantes con discapacidad que fueron parte de una nómina debe ser igual o menor al total de postulantes con discapacidad']);
+        ReglasFormularioMensaje::create(['id_regla' => '9', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°9 obligatoria: Si no hubo postulantes con discapacidad y/o asignatarios de pensión de invalidez en una nómina final indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '9', 'configuracion_mensaje'=>'min','mensaje'=>'Pregunta N°9 valor mínimo 0: Si no hubo postulantes con discapacidad y/o asignatarios de pensión de invalidez en una nómina final indicar 0']);
+        ReglasFormularioMensaje::create(['id_regla' => '9', 'configuracion_mensaje'=>'lte','mensaje'=>'Pregunta N°9: Se ha detectado una inconsistencia con el dato reportado en pregunta 8: el número de personas con discapacidad y/o asignatarias de pensión de invalidez seleccionadas debe ser igual o menor que el número de personas en nómina final']);
+        ReglasFormularioMensaje::create(['id_regla' => '10', 'configuracion_mensaje'=>'max','mensaje'=>'Pregunta N°11 excede caracteres: Campo no puede exceder los 350 caracteres']);
+
+        /** MÓDULO 3 - MEDIDAS DE ACCESIBILIDAD EN PROCESOS DE SELECCIÓN */
+        ReglasFormularioMensaje::create(['id_regla' => '11', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°12 obligatoria: Debe seleccionar una respuesta']);
+        ReglasFormularioMensaje::create(['id_regla' => '12', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°13 obligatoria: Debe seleccionar una respuesta']);
+        ReglasFormularioMensaje::create(['id_regla' => '13', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°14 obligatoria: Debe seleccionar una respuesta']);
+
+        /** MÓDULO 4 - MANTENCIÓN Y CONTRATACIÓN DE PERSONAS CON DISCAPACIDAD O ASIGNATARIAS DE PENSIÓN DE INVALIDEZ */
+        ReglasFormularioMensaje::create(['id_regla' => '14', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°15 obligatoria: Debe seleccionar una respuesta']);
+        ReglasFormularioMensaje::create(['id_regla' => '15', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°16 obligatoria: Debe seleccionar una respuesta']);
+        ReglasFormularioMensaje::create(['id_regla' => '16', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°17 obligatoria: Debe seleccionar una respuesta']);
+        ReglasFormularioMensaje::create(['id_regla' => '16', 'configuracion_mensaje'=>'max','mensaje'=>'Pregunta N°18 excede caracteres: Campo no puede exceder los 350 caracteres']);
+
+        /** MÓDULO 5 - MANTENCIÓN Y CONTRATACIÓN DE PERSONAS CON DISCAPACIDAD O ASIGNATARIAS DE PENSIÓN DE INVALIDEZ */
+        ReglasFormularioMensaje::create(['id_regla' => '17', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°19 obligatoria: Debe seleccionar una respuesta']);
+        ReglasFormularioMensaje::create(['id_regla' => '18', 'configuracion_mensaje'=>'required','mensaje'=>'Pregunta N°20 obligatoria: Debe seleccionar una respuesta']);
+        
     }
 }
--- database/seeders/ReglasFormularioSeeder.php
+++ database/seeders/ReglasFormularioSeeder.php
@@ -23,16 +23,31 @@ class ReglasFormularioSeeder extends Seeder
         */
 
         /** MÓDULO 1: IDENTIFICACIÓN DEL INFORMANTE */
-        ReglasFormulario::create(['id_formulario' => '1', 'regla'=>'required|max:1000']);
-        ReglasFormulario::create(['id_formulario' => '2', 'regla'=>'required|max:1000']);
-        ReglasFormulario::create(['id_formulario' => '3', 'regla'=>'required|max:1000|email']);
-        ReglasFormulario::create(['id_formulario' => '4', 'regla'=>'required']);
+        ReglasFormulario::create(['id_formulario' => '1', 'regla'=>'required|max:1000']); //1
+        ReglasFormulario::create(['id_formulario' => '2', 'regla'=>'required|max:1000']); //2
+        ReglasFormulario::create(['id_formulario' => '3', 'regla'=>'required|max:1000|email']); //3
+        ReglasFormulario::create(['id_formulario' => '4', 'regla'=>'required']); //4
 
         /** MÓDULO 2: SELECCIÓN PREFERENTE */
-        ReglasFormulario::create(['id_formulario' => '6', 'regla'=>'required|min:0']);
-        ReglasFormulario::create(['id_formulario' => '7', 'regla'=>'required|min:0']);
-        ReglasFormulario::create(['id_formulario' => '8', 'regla'=>'required']);
-        ReglasFormulario::create(['id_formulario' => '9', 'regla'=>'required']);
-        ReglasFormulario::create(['id_formulario' => '10', 'regla'=>'required']);
+        ReglasFormulario::create(['id_formulario' => '6', 'regla'=>'required|min:0']); //5
+        ReglasFormulario::create(['id_formulario' => '7', 'regla'=>'required|min:0']); //6
+        ReglasFormulario::create(['id_formulario' => '8', 'regla'=>'required|min:0|lte:total_postulantes_7']); //7
+        ReglasFormulario::create(['id_formulario' => '9', 'regla'=>'required|min:0|lte:total_postulantes_discapacidad_8']); //8
+        ReglasFormulario::create(['id_formulario' => '10','regla'=>'required|min:0|lte:total_postulantes_discapacidad_pension_9']); //9
+        ReglasFormulario::create(['id_formulario' => '12','regla'=>'max:350']); //10
+        
+        /** MÓDULO 3 - MEDIDAS DE ACCESIBILIDAD EN PROCESOS DE SELECCIÓN */
+        ReglasFormulario::create(['id_formulario' => '14','regla'=>'required']); //11
+        ReglasFormulario::create(['id_formulario' => '15','regla'=>'required']); //12
+        ReglasFormulario::create(['id_formulario' => '16','regla'=>'required']); //13
+
+        /** MÓDULO 4 - MANTENCIÓN Y CONTRATACIÓN DE PERSONAS CON DISCAPACIDAD O ASIGNATARIAS DE PENSIÓN DE INVALIDEZ */
+        ReglasFormulario::create(['id_formulario' => '18','regla'=>'required']); //14
+        ReglasFormulario::create(['id_formulario' => '19','regla'=>'required']); //15
+        ReglasFormulario::create(['id_formulario' => '21','regla'=>'required|max:350']); //16
+
+        /** MÓDULO 5 - MANTENCIÓN Y CONTRATACIÓN DE PERSONAS CON DISCAPACIDAD O ASIGNATARIAS DE PENSIÓN DE INVALIDEZ */
+        ReglasFormulario::create(['id_formulario' => '23','regla'=>'required']); //17
+        ReglasFormulario::create(['id_formulario' => '24','regla'=>'required']); //18
     }
 }
